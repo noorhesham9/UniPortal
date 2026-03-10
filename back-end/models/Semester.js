1b8aca5931e240ab78dbb554d688e07ca829a3b7
moaazzaki281-arch <moaazzaki281@gmail.com>
2026-03-10 23:13:53 +0200

    update Semester files

--- back-end/models/Semester.js
+++ back-end/models/Semester.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose');
 
-const semesterSchema = new mongoose.Schema({
-    year: { type: Number, required: true },
-    term: { type: String, required: true }, // مثال: Spring, Fall
-    is_active: { type: Boolean, default: false },
-    start_date: { type: Date, required: true },
-    end_date: { type: Date, required: true },
-    max_credits_rules: { type: String }, 
-    add_drop_start: { type: Date, required: true },
-    add_drop_end: { type: Date, required: true }
+const SemesterSchema = new mongoose.Schema({
+    year: {
+        type: Number,
+        required: true
+    },
+    term: {
+        type: String,
+        required: true
+    },
+    is_active: {
+        type: Boolean,
+        default: false
+    },
+    start_date: {
+        type: Date,
+        required: true
+    },
+    end_date: {
+        type: Date,
+        required: true
+    },
+    max_credits_rules: {
+        type: String
+    },
+    add_drop_start: {
+        type: Date
+    },
+    add_drop_end: {
+        type: Date
+    }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Semester', semesterSchema);
+module.exports = mongoose.model('Semester', SemesterSchema);
